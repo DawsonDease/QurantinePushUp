1099ff0bca8341c25c67dc6b63f6903de5f92f29
Dawson <DawsonDease07@gmail.com>
2020-03-31 23:04:44 -0700

    Added persistent data

--- QurantinePushUp/Entry.swift
+++ QurantinePushUp/Entry.swift
@@ -8,16 +8,44 @@
 
 import Foundation
 import UIKit
+import os.log
+
+struct PropertyKey{
+    static let date = "date"
+    static let pushups = "pushups"
+}
 
 
-class Entry {
 
+class Entry: NSObject, NSCoding{
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("entries")
     public var date : String
     public var pushups : String
     
     init(date: String, pushUps: String) {
         self.date = date
         self.pushups = pushUps
+    }
+    //MARK: NSCoding
+    func encode(with acoder: NSCoder) {
+        acoder.encode(date, forKey: PropertyKey.date)
+        acoder.encode(pushups, forKey: PropertyKey.pushups)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder){
+       guard let newDate = aDecoder.decodeObject(forKey: PropertyKey.date) as? String else{
+           os_log("Missing Date", log: OSLog.default, type: .debug)
+           return nil
+       }
+       
+       guard let newPushups = aDecoder.decodeObject(forKey: PropertyKey.pushups) as? String else {
+           os_log("Missing pushups", log: OSLog.default, type: .debug)
+           return nil
+       }
+       
+        self.init(date: newDate, pushUps: newPushups)
     }
 
 }
--- QurantinePushUp/ViewController.swift
+++ QurantinePushUp/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import os.log
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -16,7 +16,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var historyTable: UITableView!
     @IBOutlet weak var dailyRecordLabel: UILabel!
     @IBOutlet weak var currentStreakLabel: UILabel!
-    
+    var row : Int = 0
     var entries = [Entry]()
   
     
@@ -25,8 +25,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         historyTable.dataSource = self
         historyTable.delegate = self
+        entries = loadObjects() ?? entries
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: Seque
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
@@ -38,17 +41,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
         addViewController.data = entries
+        save()
         
     }
     
     @IBAction func unwindToViewController(sender:UIStoryboardSegue){
         if let sourceViewController = sender.source as? AddViewController{
             entries = sourceViewController.data
-            
-            print(entries.count)
+            save()
             historyTable.reloadData()
         }
     }
+    
+    //MARK: TableView delegates
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return entries.count
@@ -63,6 +68,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.pushups!.text = entries[indexPath.row].pushups
         return cell;
     }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            entries.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            save()
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        row = indexPath.row
+    }
+    
+    
+    
+    
+    //MARK: Load/Save
+    
+    func save(){
+       do{
+            let data = try NSKeyedArchiver.archivedData(withRootObject: entries, requiringSecureCoding: false)
+            try data.write(to: Entry.ArchiveURL)
+            historyTable.reloadData()
+        }catch{
+            os_log("Cannot save due to %@", log: OSLog.default, type: .debug, error.localizedDescription)
+        }
+    }
+    
+    func loadObjects() -> [Entry]? {
+           do{
+               let data = try Data(contentsOf: Entry.ArchiveURL)
+               return try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [Entry]
+           }catch{
+               os_log("Cannot load due to %@", log: OSLog.default, type: .debug, error.localizedDescription)
+               return nil
+           }
+       }
     
 }
 
